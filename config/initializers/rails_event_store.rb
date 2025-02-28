9f26334843371c646b4241c7e88b3206db19c453
Jianhua <48198372+Varsion@users.noreply.github.com>
2025-02-28 13:40:05 +0800

    core: use jsonb

--- config/initializers/rails_event_store.rb
+++ config/initializers/rails_event_store.rb
@@ -3,7 +3,8 @@ require "aggregate_root"
 require "arkency/command_bus"
 
 Rails.configuration.to_prepare do
-  Rails.configuration.event_store = RailsEventStore::Client.new
+  # Rails.configuration.event_store = RailsEventStore::Client.new
+  Rails.configuration.event_store = RailsEventStore::JSONClient.new
   Rails.configuration.command_bus = Arkency::CommandBus.new
 
   AggregateRoot.configure do |config|
